1b975c6fd432b9795d6d8e9a5d8f094c334766a9
ecdeng <ecdeng19@gmail.com>
2018-12-26 21:09:19 -1000

    Save the approved test images

--- image_cap_and_process.py
+++ image_cap_and_process.py
@@ -12,6 +12,7 @@ def var_lap(image):
 camera = cv2.VideoCapture(0)
 blurry = 0
 counter = 0
+counter_thres = 5
 blur_pass = False
 # threshold for var of lap--to be tuned based on image capture conditions and requirements
 thres = 5
@@ -19,12 +20,14 @@ thres = 5
 cam_id = input("Enter camera ID:")
 print("Starting camera blurriness test for camera",cam_id,"...")
 
+# Make directory to save non-blurry images to
+os.mkdir('passed_images')
 
 while blur_pass != True:
 	ret, frame = camera.read()	
 
 	# buffer image to be read and deleted
-	img_name = "buffer-frame.png"
+	img_name = "buffer-frame"+str(counter)+".png"
 	cv2.imwrite(img_name, frame)
 
 	# convert to grayscale then calculate var of laplacian
@@ -45,26 +48,22 @@ while blur_pass != True:
 	cv2.imshow("Image", image)
 	cv2.waitKey(50)
 
-
 	# Check for user input on if image is clear
 	pass_test = input("Was the image clear? (Y/N)")
 	if (pass_test=='Y') and (blurry == 0):
 		counter = counter+1
-		if counter == 5: 
+		if counter == counter_thres: 
 			blur_pass = True
 		print ("Pass counter = ",counter,".")
 	else:
 		print ("Image deemed blurry. Adjust and retry")
 		print ("Current Pass Counter = ",counter,".")
-
+		os.remove("buffer-frame"+str(counter)+".png")
 
 	blurry = 0 
 
-	os.remove("buffer-frame.png")
-
 # Instructions upon completion (, i.e. mark camera lens position or glue camera-lens assembly in place)
-print ("Do X, Y, and/or Z as the camera has now been verified. Tests2213213")
-
+print ("Do X, Y, and/or Z as the camera has now been verified.")
 
 camera.release()
 cv2.destroyAllWindows()
